035923f58bb7ec6c7d90760e129a7cce622c2ecf
Pasha <sachok.p_ipz23@rcit.ukr.education>
2025-09-30 17:33:01 +0700

    Lab 3: Inheritance1

--- lab3v15/obj/Debug/net8.0/lab3v15.AssemblyInfo.cs
+++ lab3v15/obj/Debug/net8.0/lab3v15.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3v15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c607a99b62c3dd960de9724e38746ab98dbf630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f437044232587ed2d34b82251aa66c974b34df")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3v15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3v15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
