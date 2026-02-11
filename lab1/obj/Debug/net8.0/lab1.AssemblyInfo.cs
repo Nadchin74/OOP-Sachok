093575dc858e3cc894733fcb2d8cf58566d3c30d
Pasha <sachok.p_ipz23@rcit.ukr.education>
2026-02-12 01:14:11 +0700

    docs: add report on Factory Method vs Abstract Factory

--- lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d145f6b6f307b58f5f8743ef8d57944a70149ce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539ec5927cdedaf03bb8496b89acadbac8476534")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lab3v15/obj/Debug/net8.0/lab3v15.AssemblyInfo.cs
+++ lab3v15/obj/Debug/net8.0/lab3v15.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3v15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1bb88578c9e2391eee5309d23f60cabf5947296")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539ec5927cdedaf03bb8496b89acadbac8476534")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3v15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3v15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
